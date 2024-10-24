158b8cd885c080f0f4ae12dd2e84a058973e1cec
Mariana Cruz <mariana.cruz@crazytechlabs.com.br>
2024-10-24 14:33:08 -0300

    Adding Cosmos DB vector store index

--- backend/app/index_server.py
+++ backend/app/index_server.py
@@ -1,33 +1,149 @@
+# Standard library imports
 import os
+import sys
 import pickle
-# from openai import AzureOpenAI
+import json
+import asyncio
+
+# Typing imports
+from typing import Optional, Any, Dict
+
+# Environment variable management
 from dotenv import load_dotenv
+
+# OpenAI imports
 import openai
+# from openai import AzureOpenAI
+
+# Multiprocessing imports
 from multiprocessing import Lock
 from multiprocessing.managers import BaseManager
 
+# Llama Index imports
 from llama_index.core import (
     SimpleDirectoryReader, 
     VectorStoreIndex, 
     StorageContext, 
     load_index_from_storage,
+    Settings,
+    Document
 )
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.llms.openai import OpenAI
-
-from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 
-from typing import Optional, Any, Dict
+# Azure Cosmos DB imports
+# from azure.cosmos.aio import CosmosClient as AsyncCosmos
+from azure.cosmos import PartitionKey, CosmosClient
+from llama_index.vector_stores.azurecosmosnosql import AzureCosmosDBNoSqlVectorSearch
 
-# Settings.embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-mpnet-base-v2", device="cpu")
-# Settings.llm = Ollama(model="gemma2:27b", temperature=0, request_timeout=500.0, device="cpu")
+# Azure Table Storage imports
+from llama_index.storage.docstore.azure import AzureDocumentStore
+from llama_index.storage.index_store.azure import AzureIndexStore
+from llama_index.storage.kvstore.azure.base import ServiceMode
 
+# Load environment variables
 load_dotenv()
+
+""" --- FIM DOS IMPORTS --- """
+
+"""  TESTANDO  """
+# text_list = ["text1", "text2", "..."]
+# documents = [Document(text=t) for t in text_list]
+
+# documents = SimpleDirectoryReader("docs/").load_data(True)
+
+# Ler documentos específicos:
+# documents = SimpleDirectoryReader(
+#     input_files=[r"\docs\examples\data\paul_graham\paul_graham_essay.txt"]
+# ).load_data()
+# documents = reader.load_data()
+# document = Document.example()
+# nodes = SentenceSplitter().get_nodes_from_documents(documents, True)
+# storage_context.docstore.add_documents(nodes)
+
+# print("PARTE 3")
+# index = VectorStoreIndex.from_documents(
+#     documents, storage_context, True
+# )
+
+"""  FIM DO TESTANDO  """
+
+def model_settings():
+
+    llm = AzureOpenAI(
+        model="gpt-4o-mini",
+        deployment_name="gpt-4o-mini",
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_version="2024-02-01",
+    ) 
+    Settings.llm = llm
+
+    embed_model = AzureOpenAIEmbedding(
+        model="text-embedding-3-small",
+        deployment_name="text-embedding-3-small",
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version="2024-02-01",
+    )
+    Settings.embed_model = embed_model
+    
+    Settings.text_splitter = SentenceSplitter(chunk_size=1024)
+
+def set_cosmos():
+    cosmos_config = {
+        "url": os.getenv("COSMOSDB_URL"),
+        "key": os.getenv("COSMOSDB_KEY"),
+        "database": os.getenv("DATABASE_NAME"),
+        "container": os.getenv("CONTAINER_NAME"),
+        "partition": os.getenv("PARTITION_KEY"),
+    }
+    # pprint(cosmos_config)
+    
+    cosmos_client = CosmosClient(cosmos_config["url"], cosmos_config["key"])
+
+    # specify vector store properties
+    indexing_policy = {
+        "indexingMode": "consistent",
+        "includedPaths": [{"path": "/*"}],
+        "excludedPaths": [{"path": '/"_etag"/?'}],
+        "vectorIndexes": [{"path": "/embedding", "type": "quantizedFlat"}],
+    }
+
+    vector_embedding_policy = {
+        "vectorEmbeddings": [
+            {
+                "path": "/embedding",
+                "dataType": "float32",
+                "distanceFunction": "cosine",
+                "dimensions": 3072,
+            }
+        ]
+    }
+
+    cosmos_container_properties_test = {"partition_key": PartitionKey(path = cosmos_config["partition"])}
+    cosmos_database_properties_test: Dict[str, Any] = {}
+
+    storage_context = StorageContext.from_defaults(
+        vector_store = AzureCosmosDBNoSqlVectorSearch(
+            cosmos_client=cosmos_client,
+            vector_embedding_policy=vector_embedding_policy,
+            indexing_policy=indexing_policy,
+            cosmos_container_properties=cosmos_container_properties_test,
+            cosmos_database_properties=cosmos_database_properties_test,
+            database_name = cosmos_config["database"],
+            container_name = cosmos_config["container"],
+            create_container=True,
+        )
+    )
+    print("Storage Context Ready...")
+
+    return storage_context
 
 index: Optional[Any] = None
 stored_docs: Optional[Any] = {}
@@ -42,29 +158,34 @@ def initialize_index():
     utilizing sentence splitting and embedding models.
     """
     global index, stored_docs
-    
-    transformations = SentenceSplitter(chunk_size=512)
-    # embed_model = OpenAIEmbedding(model_name="text-embedding-3-small")
-    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-mpnet-base-v2", device="cpu")
-    ## Usando o Azure
-    # embed_model = AzureOpenAIEmbedding(
-    #     model="text-embedding-ada-002",
-    #     deployment_name="my-custom-embedding",
-    #     api_key=api_key,
-    #     azure_endpoint=azure_endpoint,
-    #     api_version=api_version,
-    # )
+
+    model_settings()
+    storage_context = set_cosmos()
+
+    # --- Open Source ---
+    # embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-mpnet-base-v2", device="cpu")
+
     with lock:
-        if os.path.exists(index_name):
-            index = load_index_from_storage(
-                StorageContext.from_defaults(persist_dir=index_name), embed_model=embed_model
-            )
-        else:
-            index = VectorStoreIndex(nodes=[], embed_model=embed_model)
+        try:
+            index = VectorStoreIndex.from_vector_store(storage_context.vector_store)
+            print("Index loaded from Cosmos DB.")
+            query_index("Qual é o meu nome?")
+        except Exception as e:
+            print(f"Failed to load index from Cosmos DB: {e} \n Using local index")
+            index = VectorStoreIndex(nodes=[])
             index.storage_context.persist(persist_dir=index_name)
+            print("New index created and persisted in Cosmos DB.")
+
         if os.path.exists(pkl_name):
+            print("Loading stored docs from pickle...")
             with open(pkl_name, "rb") as f:
                 stored_docs = pickle.load(f)
+
+        # if os.path.exists(index_name):
+        #     index = load_index_from_storage(
+        #         StorageContext.from_defaults(persist_dir=index_name), 
+        #         embed_model=embed_model
+        #     )
 
 
 def query_index(query_text):
@@ -80,27 +201,15 @@ def query_index(query_text):
     and the query engine with specified parameters such as similarity_top_k.
     """
     global index
-    # llm = OpenAI(model="gpt-4o-mini")
-    # llm = Ollama(model="llama3.2:1b", temperature=0, device="cpu")
-    # response = index.as_query_engine(
-    #     similarity_top_k=2,
-    #     llm=llm,
-    # ).query(query_text)
 
-    llm = AzureOpenAI(
-        model="gpt-4o-mini",
-        deployment_name="gpt-4o-mini",
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_version="2024-02-01",
-    )
-    
+    model_settings()
+
     query_engine = index.as_query_engine(
         similarity_top_k=2,
-        llm=llm,
     )
     response = query_engine.query(query_text)
-    
+    print(response)
+
     return response
 
 
@@ -148,7 +257,7 @@ def get_documents_list():
     :return: The function `get_documents_list` returns a list of dictionaries, where each dictionary
     contains the id and text of a document stored in the `stored_docs` dictionary.
     """
-    global stored_doc
+    global stored_docs
     documents_list = []
     for doc_id, doc_text in stored_docs.items():
         documents_list.append({"id": doc_id, "text": doc_text})
@@ -175,8 +284,28 @@ def get_pid_using_port(port):
 
 import time
 from tqdm import tqdm
+import signal, psutil
+
+def kill_task(port):
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            for conn in proc.net_connections(kind='inet'):
+                if conn.laddr.port == port:
+                    proc.kill()
+                    print(f"Processo {proc.info['name']} (PID {proc.info['pid']}) na porta {port} foi terminado.")
+                    return
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+    print(f"Nenhum processo encontrado na porta {port}.")
+
+def signal_handler(sig, frame):
+    print("Ctrl+C pressionado. Matando a tarefa...")
+    kill_task(5002)
+    exit(0)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    
     # init the global index
     print("initializing index...")
     initialize_index()
